88914c35f7b36029669d97c03e74975d97fd50fc
aven90 <>
2024-12-01 13:52:38 +0100

    refactor update

--- day1/day1.py
+++ day1/day1.py
@@ -2,21 +2,19 @@ import pandas as pd
 
 
 # Part 1
-df_input = pd.read_csv("day1.csv", sep=";")
+df_input = pd.read_csv("day1/day1.csv", sep=";")
 
 print(df_input)
 
 list_col1 = df_input["col1"].tolist()
 list_col2 = df_input["col2"].tolist()
 
-list_col1.sort()
-list_col2.sort()
+list_col1.sort(), list_col2.sort()
 
 df_calc = pd.DataFrame({"col1": list_col1, "col2": list_col2})
-
 df_calc["diff"] = abs(df_calc["col1"] - df_calc["col2"])
-
 total = df_calc["diff"].sum()
+
 print(f"The total is: {total}")
 
 
@@ -24,7 +22,6 @@ print(f"The total is: {total}")
 # check how often each num occurs in the list
 
 count = dict()
-
 for num in list_col1:
     count[num] = list_col2.count(num)
 
@@ -34,5 +31,3 @@ df_filt = df_p2.query("count != 0").reset_index()
 df_p2["mult"] = df_p2["num"] * df_p2["count"]
 total_p2= df_p2["mult"].sum()
 print(f"The total is: {total_p2}")
-
-
